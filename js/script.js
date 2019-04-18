1daf3f826c05367ba94027326f92fb3954f09342
Zoe Teoh <zteoh@andrew.cmu.edu>
2019-04-18 16:24:27 -0400

    added stripe payments and drift chat code

--- js/script.js
+++ js/script.js
@@ -237,3 +237,167 @@ function validate() {
         }
     }
 }
+
+//Stripe Things
+
+// Create a Stripe client.
+var stripe = Stripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
+
+//// Create an instance of Elements.
+//var elements = stripe.elements();
+//
+//// Custom styling can be passed to options when creating an Element.
+//// (Note that this demo uses a wider set of styles than the guide below.)
+//var style = {
+//  base: {
+//    color: '#32325d',
+//    fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+//    fontSmoothing: 'antialiased',
+//    fontSize: '16px',
+//    '::placeholder': {
+//      color: '#aab7c4'
+//    }
+//  },
+//  invalid: {
+//    color: '#fa755a',
+//    iconColor: '#fa755a'
+//  }
+//};
+//
+//// Create an instance of the card Element.
+//var card = elements.create('card', {style: style});
+//
+//// Add an instance of the card Element into the `card-element` <div>.
+//card.mount('#card-element');
+//
+//// Handle real-time validation errors from the card Element.
+//card.addEventListener('change', function(event) {
+//  var displayError = document.getElementById('card-errors');
+//  if (event.error) {
+//    displayError.textContent = event.error.message;
+//  } else {
+//    displayError.textContent = '';
+//  }
+//});
+//
+//// Handle form submission.
+//var form = document.getElementById('payment-form');
+//form.addEventListener('submit', function(event) {
+//  event.preventDefault();
+//
+//  stripe.createToken(card).then(function(result) {
+//    if (result.error) {
+//      // Inform the user if there was an error.
+//      var errorElement = document.getElementById('card-errors');
+//      errorElement.textContent = result.error.message;
+//    } else {
+//      // Send the token to your server.
+//      stripeTokenHandler(result.token);
+//    }
+//  });
+//});
+//
+//// Submit the form with the token ID.
+//function stripeTokenHandler(token) {
+//  // Insert the token ID into the form so it gets submitted to the server
+//  var form = document.getElementById('payment-form');
+//  var hiddenInput = document.createElement('input');
+//  hiddenInput.setAttribute('type', 'hidden');
+//  hiddenInput.setAttribute('name', 'stripeToken');
+//  hiddenInput.setAttribute('value', token.id);
+//  form.appendChild(hiddenInput);
+//
+//  // Submit the form
+//  form.submit();
+//}
+
+//Stripe Example 5
+
+(function() {
+  "use strict";
+
+  var elements = stripe.elements({
+    // Stripe's examples are localized to specific languages, but if
+    // you wish to have Elements automatically detect your user's locale,
+    // use `locale: 'auto'` instead.
+    locale: window.__exampleLocale
+  });
+
+  /**
+   * Card Element
+   */
+  var card = elements.create("card", {
+    iconStyle: "solid",
+    style: {
+      base: {
+        iconColor: "#fff",
+        color: "#fff",
+        fontWeight: 400,
+        fontFamily: "Helvetica Neue, Helvetica, Arial, sans-serif",
+        fontSize: "16px",
+        fontSmoothing: "antialiased",
+
+        "::placeholder": {
+          color: "#BFAEF6"
+        },
+        ":-webkit-autofill": {
+          color: "#fce883"
+        }
+      },
+      invalid: {
+        iconColor: "#FFC7EE",
+        color: "#FFC7EE"
+      }
+    }
+  });
+  card.mount("#example5-card");
+
+  /**
+   * Payment Request Element
+   */
+  var paymentRequest = stripe.paymentRequest({
+    country: "US",
+    currency: "usd",
+    total: {
+      amount: 2500,
+      label: "Total"
+    },
+    requestShipping: true,
+    shippingOptions: [
+      {
+        id: "free-shipping",
+        label: "Free shipping",
+        detail: "Arrives in 5 to 7 days",
+        amount: 0
+      }
+    ]
+  });
+  paymentRequest.on("token", function(result) {
+    var example = document.querySelector(".example5");
+    example.querySelector(".token").innerText = result.token.id;
+    example.classList.add("submitted");
+    result.complete("success");
+  });
+
+  var paymentRequestElement = elements.create("paymentRequestButton", {
+    paymentRequest: paymentRequest,
+    style: {
+      paymentRequestButton: {
+        theme: "light"
+      }
+    }
+  });
+
+  paymentRequest.canMakePayment().then(function(result) {
+    if (result) {
+      document.querySelector(".example5 .card-only").style.display = "none";
+      document.querySelector(
+        ".example5 .payment-request-available"
+      ).style.display =
+        "block";
+      paymentRequestElement.mount("#example5-paymentRequest");
+    }
+  });
+
+  registerElements([card], "example5");
+})();
